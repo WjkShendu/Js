20249b48b4a0b55b79e219dad57df015b78010da
王景逵 <48682216+WjkShendu@users.noreply.github.com>
2019-04-11 19:43:25 +0800

    Add files via upload

---
+++ 2019-4-10面向对象_原型_继承/案例篇/js/Object.js
@@ -0,0 +1,22 @@
+function Tab(id){
+	console.log(this)
+	var tabBox = document.getElementById(id);
+	this.tabBtn = tabBox.getElementsByTagName("input");
+	this.tabDiv = tabBox.getElementsByTagName("div");
+	for (var i = 0 ; i < this.tabBtn.length ; i++) {
+		this.tabBtn[i].index = i;
+		var obj = this;
+		this.tabBtn[i].onclick = function(){
+			obj.click(this);
+		}
+	}
+}
+Tab.prototype.click = function(btn){
+	console.log(this)
+	for (var i = 0 ; i < this.tabBtn.length ; i++) {
+		this.tabBtn[i].className = "";
+		this.tabDiv[i].className = "";
+	}
+	btn.className = "active";
+	this.tabDiv[btn.index].className = "act";
+}
